f59de9b64f2862037118f280a0be218d035f1133
Your Name <mmhamdyyy@gmail.com>
2026-06-19 16:34:32 +0300

    Add post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
-class Post < ApplicationRecord
+class Post
+  include ActiveModel::Model
+  attr_accessor :title, :description, :likes
+  def self.all 
+    [
+      new(title: "welcome with rails", description: "this is a nice course", likes: "10k")
+    ]
+  end
 end
