b6ae4e197bf78adb97529dd6e01cba986350c6a9
asaadmansour <as89aadmansour@gmail.com>
2026-06-20 23:50:57 +0300

    added post's controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,35 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  # /posts/new
+  def new 
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect_to @post, notice: "post created!"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post,notice: "post updated!"
+    else 
+      render :new, status: :unprocessable_content
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "post deleted!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/posts", to: "posts#index"
+  get "/posts/new", to: "posts#new", as: :new_post
   post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: :post
+  get "/posts/:id/edit", to: "posts#edit", as: :edit_post
+  patch "/posts/:id", to: "posts#update"
+  delete "/posts/:id", to: "posts#destroy"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
