71904a22fcf30080537a683fa15954bbfe175473
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-06-20 18:36:26 +0300

    Add User validations: presence, email format, email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: true
 end
