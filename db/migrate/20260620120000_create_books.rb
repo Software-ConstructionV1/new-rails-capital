df702a1178298c4475067c6abebe76ffca063aa1
Ahmed Ehab <ahmedehab62003@gmail.com>
2026-06-20 11:06:53 +0300

    Add books migration for new resource

---
+++ db/migrate/20260620120000_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[8.1]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
