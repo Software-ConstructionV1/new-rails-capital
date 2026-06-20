d5d4ba92d05cda2666bed6e7ed2c536a76f78121
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-06-20 16:39:56 +0300

    add users backend level constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	validates :name, :dob, :email, :phone_number, presence: true
+	
+	validates :email, format: {
+		# note: I might have gone a little excessive with the allowed stuff
+		# but this is basic regex from the internet so I guess it's fine
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, 
+    message: "must be a valid email address"
+  }
+
+	validates :email, uniqueness: { case_sensitive: false }
 end
