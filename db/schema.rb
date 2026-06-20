438769f39ac0e041884866ebf8e47bc79e6a7663
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-06-20 15:17:38 +0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_083257) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_20_121416) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.date "dob"
+    t.string "email"
+    t.string "name"
+    t.string "phone_number"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
