f957918894cf0de782ebc0945c07a65d636c8f4d
muhanadmedhat <muhanadronaldo@yahoo.com>
2026-06-20 22:04:07 +0300

    Finished controller of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,43 @@
 class PostsController < ApplicationController
   # your code goes here
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect_to @post, notice: "Post Created"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post, notice: "Post Updated"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Deleted Successfully"
+  end
 end
