1f530d4f759b32d113544fd572e4dd52cce32601
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-06-20 16:38:13 +0300

    added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,49 @@
 class UsersController < ApplicationController
-  "Your code goes here"
+	before_action :set_user, only: %i[ show edit update destroy ]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "User destroyed!"
+  end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+    params.require(:user).permit(:name, :email, :dob, :phone_number)
+  end
+
 end
