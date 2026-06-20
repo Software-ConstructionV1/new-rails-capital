0366a65ddd7d767c6ac4552fa5ac23441d0bad31
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-06-20 18:36:26 +0300

    Implement PostsController: 7 CRUD actions with endpoint URL comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,57 @@
 class PostsController < ApplicationController
-  # your code goes here
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # POST /posts/commit
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/:id
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy!
+    redirect_to posts_path, notice: "Post was successfully destroyed.", status: :see_other
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
