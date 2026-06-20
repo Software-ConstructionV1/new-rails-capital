817c3ae55b3294830e36b4653d134de081d155fd
Mostafa-Khalifaa <mostafa.khalifa.2000.mk@gmail.com>
2026-06-20 17:29:13 +0300

    Add address field to User form and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :name, :dob, :email, :phone_number, presence: true
+  validates :name, :dob, :email, :phone_number, :address, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
 end
