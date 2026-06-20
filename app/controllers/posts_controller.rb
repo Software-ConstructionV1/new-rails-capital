d9d1b3be3589c14e72a188a770b03eabedf7369a
muhanadmedhat <muhanadronaldo@yahoo.com>
2026-06-21 01:31:58 +0300

    Finished user controller and validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
   # your code goes here
 
+  # GET /posts
   def index
     @posts = Post.all
   end
 
+  # GET /posts/new
   def new
     @post = Post.new
   end
 
+  # POST /posts
   def create
     @post = Post.new(title: params[:post][:title], content: params[:post][:content])
     if @post.save
@@ -18,14 +21,17 @@ class PostsController < ApplicationController
     end
   end
 
+  # GET /posts/:id
   def show
     @post = Post.find(params[:id])
   end
 
+  # GET /posts/:id/edit
   def edit
     @post = Post.find(params[:id])
   end
 
+  # PATCH /posts/:id
   def update
     @post = Post.find(params[:id])
     if @post.update(title: params[:post][:title], content: params[:post][:content])
@@ -35,6 +41,7 @@ class PostsController < ApplicationController
     end
   end
 
+  # DELETE /posts/:id
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,41 @@
 class UsersController < ApplicationController
-  "Your code goes here"
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number], address: params[:user][:address])
+    if @user.save
+      redirect_to @user, notice: "User created"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number], address: params[:user][:address])
+      redirect_to @user, notice: "User Updated"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "Deleted Successfully"
+  end
 end
