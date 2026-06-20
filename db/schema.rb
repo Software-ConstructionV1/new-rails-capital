b3b5f69e80c88f46940eb93cc2776999dd9c91ab
Your Name <mmhamdyyy@gmail.com>
2026-06-20 19:45:35 +0300

    database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_20_064625) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_20_082854) do
   create_table "posts", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", null: false
     t.integer "likes"
     t.string "title"
@@ -19,9 +20,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_20_064625) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "age"
     t.datetime "created_at", null: false
+    t.date "dob"
+    t.string "email"
     t.string "name"
+    t.string "phone_number"
     t.datetime "updated_at", null: false
   end
 end
