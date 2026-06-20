44c8930dbcb9f7dbc0b44ad04a12a0f91e3902f9
john-roufaeil <john.f.roufaeil@gmail.com>
2026-06-20 17:13:35 +0300

    feat: add posts controllers with route comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,49 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new (form view)
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    end
+  end
+
+  # GET /posts/:id/edit (form view)
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH or PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
