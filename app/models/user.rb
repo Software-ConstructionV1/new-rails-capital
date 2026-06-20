416bc164865a85d41f740962679425e849fab3aa
Khaleddd11 <161937385+Khaleddd11@users.noreply.github.com>
2026-06-20 18:29:05 +0300

    Complete User resource with validations and CRUD actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :email, presence: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" },
+                    uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true
 end
