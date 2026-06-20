812002640566ccf3011f7400edaf0a92e5288c57
john-roufaeil <john.f.roufaeil@gmail.com>
2026-06-20 19:52:20 +0300

    feat: add address column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_20_142411) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_20_165120) do
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "duration"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_20_142411) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.date "dob"
     t.string "email"
