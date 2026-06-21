36020f08f6737e438c0d0c8bf1f166423657548a
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-06-21 18:22:07 +0300

    fix some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :creators, class_name: "Post"
-  has_many :post_editings, class_name: "PostEditing"
+  has_many :posts, foreign_key: "creator_id"
+  validates :name, presence: true
+  validates :email, presence: true
+  has_many :post_editings, foreign_key: "post_editor_id"
   has_many :edited_posts, through: :post_editings, source: :edited_post
 end
