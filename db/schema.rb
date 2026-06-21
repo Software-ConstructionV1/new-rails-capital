ae3264992adf5f2cc6f5286e707deb5de1b2db58
Ged0oo <mohmmednagy@gmail.com>
2026-06-21 15:02:03 +0300

    update scaffold setup

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_06_21_115144) do
+  create_table "post_editors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "editor_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["editor_id"], name: "index_post_editors_on_editor_id"
+    t.index ["post_id"], name: "index_post_editors_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.integer "creator_id"
+    t.integer "editor_id"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["creator_id"], name: "index_posts_on_creator_id"
+    t.index ["editor_id"], name: "index_posts_on_editor_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.date "dob"
+    t.string "email"
+    t.string "name"
+    t.string "phone_number"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "post_editors", "posts"
+  add_foreign_key "post_editors", "users", column: "editor_id"
+  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "creator_id"
+  add_foreign_key "posts", "users", column: "editor_id"
+end
