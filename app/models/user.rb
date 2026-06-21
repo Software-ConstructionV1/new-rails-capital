4119c660ca4e924a70d0edd023f31a9014a25fba
Shahd Mostafa <shahd.mostafa3711@gmail.com>
2026-06-21 08:14:39 +0300

    Add User model validations: presence, email format, email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :email, presence: true,
+                     uniqueness: { case_sensitive: false },
+                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :phone_number, presence: true
 end
