f30241128740d42f6926969ea0db14fd5fded57e
Abdulrahman <movieshome160@gmail.com>
2026-06-21 07:55:08 +0300

    feat: implement CRUD actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,48 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # get /posts
+  def index
+    @posts = Post.all()
+  end
+
+  # get /posts/new -> shows the form view
+  def new
+    @post = Post.new()
+  end
+
+  # post /posts
+  def create
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save()
+      redirect_to @post, notice: "product has been created!!!"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  # get /posts/id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # get /posts/id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # patch /posts/id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post, notice: "Product has been updated succesfully"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  # delete /posts/id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy()
+    redirect_to @post, notice: "Post is deleted!!!"
+  end
 end
