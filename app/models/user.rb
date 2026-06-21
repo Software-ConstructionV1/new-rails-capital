4055fd7b83b3666d8ee47dc9df6a7cc2ef6d01fe
Abdulrahman <movieshome160@gmail.com>
2026-06-21 07:58:07 +0300

    feat: add validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :dob, :email, :phone_number, :address
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
