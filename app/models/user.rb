29938e06e0d65f0d0511c058399e21b4caabbe3f
HashimAbdulaziz <hashimzezo222@gmail.com>
2026-06-21 06:55:22 +0300

    added Validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :DOB, :email, :phone_number, presence: true
+
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
