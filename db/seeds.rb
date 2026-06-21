e001d598b22397c7a12821d3e25b99f9fcd97e70
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-06-21 18:22:14 +0300

    add seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+Post.destroy_all
+PostEditing.destroy_all
+
+
+ahmed = User.create!(name: "Ahmed", email: "ahmed@email.com")
+mohamed = User.create!(name: "Mohamed", email: "mohamed@email.com")
+
+
+post1 = Post.create!(title: "T1",   content: "zvcxzcvx.", creator: ahmed)
+
+post2 = mohamed.posts.create!(title: "T2", content: "qweqwewq")
+
+ahmed.post_editings.create(edited_post: post1)
+mohamed.post_editings.create(edited_post: post1)
