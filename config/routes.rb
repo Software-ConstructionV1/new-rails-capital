8ac430be96ac46ec2566cea112ab1a080cd3396e
Shahd Mostafa <shahd.mostafa3711@gmail.com>
2026-06-23 03:27:10 +0300

    Add comments route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :articles
+  resources :comments
   get "up" => "rails/health#show", as: :rails_health_check
 end
