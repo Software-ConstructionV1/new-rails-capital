3d154d2c58ed10c34902b4a8e6539981b06bd187
HashimAbdulaziz <hashimzezo222@gmail.com>
2026-06-24 12:25:50 +0300

    feat: scaffold User and Post 1:M association

--- db/migrate/20260624092507_create_posts.rb
+++ db/migrate/20260624092507_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[8.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :content
+      t.text :body
+      t.references :creator, null: false, foreign_key: true
 
       t.timestamps
     end
